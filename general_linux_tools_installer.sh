6d3611da19f5d9c6c101356cf4fe527283a1d788
Navjot Singh <weavebytes@gmail.com>
2017-02-22 00:36:08 +0530

    general linux tools etc needed to install after fresh linux os installation

---
+++ general_linux_tools_installer.sh
@@ -0,0 +1,67 @@
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing terminator                   |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+
+sudo add-apt-repository ppa:gnome-terminator
+sudo apt-get update
+sudo apt-get install terminator -y
+
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing GVIM                         |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo apt-get install vim-gnome -y
+
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing uget download manager        |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo add-apt-repository ppa:plushuang-tw/uget-stable
+sudo apt update
+sudo apt install uget -y
+
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing tree                         |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo apt-get install tree -y
+
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing git and gitg                 |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo apt-get install git gitg -y
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing iptux                        |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo apt-get install iptux -y
+
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing pip                          |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo apt install python-pip -y
+
+
+echo "+-------------------------------------------+"
+echo "|                                           |"
+echo "|   Installing vlc                          |"
+echo "|                                           |"
+echo "+-------------------------------------------+"
+sudo apt-get git vlc 
+
